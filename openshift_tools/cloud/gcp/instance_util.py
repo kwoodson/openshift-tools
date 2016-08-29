ab34906597a3996e89726f2536199f01172ea3fc
Kenny Woodson <kwoodson@redhat.com>
2016-08-29 17:26:14 -0400

    Adding gcp label and snapshot code.

---
+++ openshift_tools/cloud/gcp/instance_util.py
@@ -0,0 +1,23 @@
+# vim: expandtab:tabstop=4:shiftwidth=4
+
+"""
+ This is a library for gcp instance utility methods.
+"""
+
+
+from openshift_tools.cloud.gcp.base import Base
+
+class InstanceUtil(Base):
+    """ Useful utility methods for instances """
+
+    def __init__(self, project, region, creds_path=None, verbose=False):
+        """ Initialize the class """
+        super(InstanceUtil, self).__init__(project, region, creds_path, verbose)
+
+    def get_all_instances_as_dict(self):
+        """ Returns a disctionary of all instances where the key is the instance name """
+        retval = {}
+        for inst in self.instances:
+            retval[inst['name']] = inst
+
+        return retval
